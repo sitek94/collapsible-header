69294187b786f9aa4fe193759d5d04c5d1f253df
Maciek Sitkowski <msitkowski94@gmail.com>
2024-09-29 13:02:38 +0200

    create wrapper header component

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,13 @@
 import './app.css'
 
-import {useState} from 'react'
-import {Menu, MenuButton, MenuItem, MenuItems} from '@headlessui/react'
+import {
+  createContext,
+  PropsWithChildren,
+  useContext,
+  useId,
+  useState,
+} from 'react'
+import {Popover, PopoverButton, PopoverPanel} from '@headlessui/react'
 
 export function App() {
   const [isHeaderCollapsed, setIsHeaderCollapsed] = useState(false)
@@ -10,7 +16,13 @@ export function App() {
 
   return (
     <>
-      <Header isCollapsed={isHeaderCollapsed} />
+      <Header isCollapsed={isHeaderCollapsed}>
+        <HeaderSection title="General">
+          <button>Add</button>
+          <button>Edit</button>
+          <button>Remove</button>
+        </HeaderSection>
+      </Header>
 
       <hr />
 
@@ -21,39 +33,49 @@ export function App() {
   )
 }
 
-function Header({isCollapsed = false}) {
+const HeaderContext = createContext({
+  isCollapsed: false,
+})
+
+const Header = ({
+  children,
+  isCollapsed = false,
+}: PropsWithChildren & {isCollapsed: boolean}) => {
+  return (
+    <HeaderContext.Provider value={{isCollapsed}}>
+      <header>{children}</header>
+    </HeaderContext.Provider>
+  )
+}
+
+const useHeaderContext = () => {
+  return useContext(HeaderContext)
+}
+
+const HeaderSection = ({
+  title,
+  children,
+}: PropsWithChildren & {title: string}) => {
+  const titleId = useId()
+  const {isCollapsed} = useHeaderContext()
+
   if (isCollapsed) {
     return (
-      <header>
-        <Menu as="section">
-          <MenuButton>My account</MenuButton>
-          <MenuItems anchor="bottom" className="menu-items">
-            <MenuItem>
-              <a href="/settings">Settings</a>
-            </MenuItem>
-            <MenuItem>
-              <a href="/support">Support</a>
-            </MenuItem>
-            <MenuItem>
-              <a href="/license">License</a>
-            </MenuItem>
-          </MenuItems>
-        </Menu>
-      </header>
+      <Popover>
+        <PopoverButton>{title}</PopoverButton>
+        <PopoverPanel anchor="bottom" className="header-section-popover-panel">
+          {children}
+        </PopoverPanel>
+      </Popover>
     )
   }
 
   return (
-    <header>
-      <section className="general">
-        <h2 className="title">General</h2>
-
-        <div className="buttons">
-          <button>Add</button>
-          <button>Edit</button>
-          <button>Remove</button>
-        </div>
-      </section>
-    </header>
+    <section aria-labelledby={titleId}>
+      <h2 id={titleId} className="title">
+        {title}
+      </h2>
+      <div className="items">{children}</div>
+    </section>
   )
 }
