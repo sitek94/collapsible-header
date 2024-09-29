0007e2f51eb5ab785ee093629fe8b9aa313df66f
Maciek Sitkowski <msitkowski94@gmail.com>
2024-09-29 12:36:30 +0200

    leave only general section temporarily

--- src/app.tsx
+++ src/app.tsx
@@ -54,22 +54,6 @@ function Header({isCollapsed = false}) {
           <button>Remove</button>
         </div>
       </section>
-
-      <section className="general">
-        <h2 className="title">General</h2>
-
-        <div className="buttons">
-          <button>Add</button>
-          <button>Edit</button>
-          <button>Remove</button>
-        </div>
-      </section>
-
-      <section className="layout">
-        <h2 className="title">Search</h2>
-
-        <input type="text" placeholder="Search..." />
-      </section>
     </header>
   )
 }
