aa2c4121bd0f47811a9739a07ea064195d7150d6
Maciek Sitkowski <msitkowski94@gmail.com>
2024-09-29 13:57:30 +0200

    use material-ui v4

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,24 @@
-import './app.css'
-
+import {
+  AppBar,
+  Button,
+  ClickAwayListener,
+  IconButton,
+  MenuList,
+  Paper,
+  Popper,
+  Toolbar,
+  Typography,
+} from '@material-ui/core'
+import {KeyboardArrowDown, KeyboardArrowUp} from '@material-ui/icons'
 import {
   createContext,
   PropsWithChildren,
   useContext,
   useId,
+  useRef,
   useState,
 } from 'react'
-import {Popover, PopoverButton, PopoverPanel} from '@headlessui/react'
+import type React from 'react'
 
 export function App() {
   const [isHeaderCollapsed, setIsHeaderCollapsed] = useState(false)
@@ -16,19 +27,13 @@ export function App() {
 
   return (
     <>
-      <Header isCollapsed={isHeaderCollapsed}>
+      <Header isCollapsed={isHeaderCollapsed} toggle={toggleHeader}>
         <HeaderSection title="General">
           <button>Add</button>
           <button>Edit</button>
           <button>Remove</button>
         </HeaderSection>
       </Header>
-
-      <hr />
-
-      <main>
-        <button onClick={toggleHeader}>Toggle Header</button>
-      </main>
     </>
   )
 }
@@ -40,10 +45,22 @@ const HeaderContext = createContext({
 const Header = ({
   children,
   isCollapsed = false,
-}: PropsWithChildren & {isCollapsed: boolean}) => {
+  toggle,
+}: PropsWithChildren & {isCollapsed: boolean; toggle: () => void}) => {
   return (
     <HeaderContext.Provider value={{isCollapsed}}>
-      <header>{children}</header>
+      <AppBar position="static" color="transparent">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            style={{marginRight: '1rem '}}
+            onClick={toggle}
+          >
+            {isCollapsed ? <KeyboardArrowDown /> : <KeyboardArrowUp />}
+          </IconButton>
+          {children}
+        </Toolbar>
+      </AppBar>
     </HeaderContext.Provider>
   )
 }
@@ -57,24 +74,73 @@ const HeaderSection = ({
   children,
 }: PropsWithChildren & {title: string}) => {
   const titleId = useId()
+  const panelId = useId()
   const {isCollapsed} = useHeaderContext()
+
+  const [open, setOpen] = useState(false)
+  const anchorRef = useRef<HTMLButtonElement>(null)
+
+  const handleToggle = () => {
+    setOpen(prevOpen => !prevOpen)
+  }
+
+  const handleClose = (event: React.MouseEvent<Document, MouseEvent>) => {
+    if (
+      anchorRef.current &&
+      anchorRef.current.contains(event.target as HTMLElement)
+    ) {
+      return
+    }
+
+    setOpen(false)
+  }
+
+  function handleListKeyDown(event: React.KeyboardEvent) {
+    if (event.key === 'Tab') {
+      event.preventDefault()
+      setOpen(false)
+    }
+  }
 
   if (isCollapsed) {
     return (
-      <Popover>
-        <PopoverButton>{title}</PopoverButton>
-        <PopoverPanel anchor="bottom" className="header-section-popover-panel">
-          {children}
-        </PopoverPanel>
-      </Popover>
+      <div>
+        <Button
+          ref={anchorRef}
+          aria-controls={open ? panelId : undefined}
+          aria-haspopup="true"
+          onClick={handleToggle}
+        >
+          {title}
+        </Button>
+
+        <Popper
+          open={open}
+          anchorEl={anchorRef.current}
+          role={undefined}
+          disablePortal
+        >
+          <Paper>
+            <ClickAwayListener onClickAway={handleClose}>
+              <MenuList
+                autoFocusItem={open}
+                id="menu-list-grow"
+                onKeyDown={handleListKeyDown}
+              >
+                {children}
+              </MenuList>
+            </ClickAwayListener>
+          </Paper>
+        </Popper>
+      </div>
     )
   }
 
   return (
     <section aria-labelledby={titleId}>
-      <h2 id={titleId} className="title">
+      <Typography component="h2" variant="subtitle2" id={titleId} gutterBottom>
         {title}
-      </h2>
+      </Typography>
       <div className="items">{children}</div>
     </section>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
-import './index.css'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import CssBaseline from '@material-ui/core/CssBaseline'
 
 import {App} from './app.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <>
+    <CssBaseline />
     <App />
-  </React.StrictMode>,
+  </>,
 )
