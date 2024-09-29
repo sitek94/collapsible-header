7e115763f890128bf61c5b36d5772e82f8cf56b1
Maciek Sitkowski <msitkowski94@gmail.com>
2024-09-29 12:35:54 +0200

    add simple layout and styles

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,75 @@
+import './app.css'
+
+import {useState} from 'react'
+import {Menu, MenuButton, MenuItem, MenuItems} from '@headlessui/react'
+
 export function App() {
+  const [isHeaderCollapsed, setIsHeaderCollapsed] = useState(false)
+
+  const toggleHeader = () => setIsHeaderCollapsed(!isHeaderCollapsed)
+
   return (
     <>
-      <h1>App</h1>
+      <Header isCollapsed={isHeaderCollapsed} />
+
+      <hr />
+
+      <main>
+        <button onClick={toggleHeader}>Toggle Header</button>
+      </main>
     </>
+  )
+}
+
+function Header({isCollapsed = false}) {
+  if (isCollapsed) {
+    return (
+      <header>
+        <Menu as="section">
+          <MenuButton>My account</MenuButton>
+          <MenuItems anchor="bottom" className="menu-items">
+            <MenuItem>
+              <a href="/settings">Settings</a>
+            </MenuItem>
+            <MenuItem>
+              <a href="/support">Support</a>
+            </MenuItem>
+            <MenuItem>
+              <a href="/license">License</a>
+            </MenuItem>
+          </MenuItems>
+        </Menu>
+      </header>
+    )
+  }
+
+  return (
+    <header>
+      <section className="general">
+        <h2 className="title">General</h2>
+
+        <div className="buttons">
+          <button>Add</button>
+          <button>Edit</button>
+          <button>Remove</button>
+        </div>
+      </section>
+
+      <section className="general">
+        <h2 className="title">General</h2>
+
+        <div className="buttons">
+          <button>Add</button>
+          <button>Edit</button>
+          <button>Remove</button>
+        </div>
+      </section>
+
+      <section className="layout">
+        <h2 className="title">Search</h2>
+
+        <input type="text" placeholder="Search..." />
+      </section>
+    </header>
   )
 }
